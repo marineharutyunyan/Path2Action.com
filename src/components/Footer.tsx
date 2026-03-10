56d925a93006be9a6fc8ab2398465eb43bec1347
Marine.Harutyunyan <marine.harutyunyan@helpsystems.com>
2026-03-10 12:37:59 +0400

    updated sizes of logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,8 +36,8 @@ export const Footer = () => {
 
   const partners = [
     { src: civicusLogo, alt: "CIVICUS", height: "h-14 md:h-16" },
-    { src: metamorphosisLogo, alt: "Metamorphosis Foundation", height: "h-14 md:h-16" },
-    { src: cliLogo, alt: "Civic Literacy Initiative", height: "h-14 md:h-16" },
+    { src: metamorphosisLogo, alt: "Metamorphosis Foundation", height: "h-18 md:h-20" },
+    { src: cliLogo, alt: "Civic Literacy Initiative", height: "h-18 md:h-20" },
     { src: apyLogo, alt: "APY", height: "h-14 md:h-16" },
   ];
 
